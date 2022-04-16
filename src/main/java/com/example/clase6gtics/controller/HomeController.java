c4916236a2e7c29a4fea49f5cc4e0eaa3357bbe6
Stuardo Lucho Hp Omen <stuardo.lucho@pucp.edu.pe>
2022-04-16 12:24:02 -0500

    postvideos flipped

--- src/main/java/com/example/clase6gtics/controller/HomeController.java
+++ src/main/java/com/example/clase6gtics/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.clase6gtics.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,6 +10,29 @@ public class HomeController {
     @GetMapping("/")
     public String index() {
         return "redirect:/product";
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm(){
+        return "login/form";
+    }
+
+    @GetMapping("/redirectByRole")
+    public String redirectByRole(Authentication auth){
+
+        String role = "";
+        for (GrantedAuthority authority : auth.getAuthorities()) {
+            role = authority.getAuthority();
+            break;
+        }
+
+        if(role.equals("admin")){
+            return "redirect:/shipper";
+        }else{
+            return "redirect:/employee";
+        }
+
+
     }
 }
 
